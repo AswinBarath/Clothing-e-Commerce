7ceefbb0d07b072ca1f8b40853183b7738d1dc71
AswinBarath <aswin2001barath@gmail.com>
2021-10-08 18:28:03 +0530

    Updated menu-items to navigate using withRouter HOC
    The menu-items component is automated to route to specific pages based on the passed props, and these props are utilized properly by the chilren using the Higher Order Component (HOC) provided by the React Router called - withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// withRouter is a higher-order component 
+// ( A function which takes component as input and return modified component as output )
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div 
                 className='background-image'
                 style={{
@@ -19,4 +23,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
